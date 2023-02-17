cf5afc41f7b9372fd2fb851bb98ec3ed5392e205
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-02-17 16:52:05 +0900

    성적처리 3 - 상적리스트 출력2

--- models/Sungjuk.js
+++ models/Sungjuk.js
@@ -6,7 +6,10 @@ class SungJuk {
         outFormat: oracledb.OUT_FORMAT_OBJECT
     };
 
-    selectsql = ' select sjno,name,kor,eng,mat,to_char(regdate,\'YYYY-MM-DD\' ) regdate from sungjuk order by sjno desc ';
+    selectsql = ` select sjno,name,kor,eng,mat, ` +
+        ` to_char(regdate, 'YYYY-MM-DD') regdate from sungjuk ` +
+        ` order by sjno desc `;
+
     // 생성자 정의 - 변수 초기화
     // 즉, 매개변수로 전달된 값을 클래스 멤버변수에 대입함
     constructor(name,kor,eng,mat,tot,avg,grd) {
@@ -43,11 +46,11 @@ class SungJuk {
     async select() {
         let conn = null;
         let result = null;
+        let sjs = [];
 
         try {
             conn = await oracledb.makeConn();
-            result = await conn.execute(
-                this.selectsql, [], this.options);
+            result = await conn.execute(this.selectsql, [], this.options);
 
             let rs = result.resultSet;
             let row = null;
