8532687a353380d57ba92a49dda1f7ac34734740
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-02-17 11:03:23 +0900

    성적처리 2 - 성적처리 후 디비 저장

---
+++ dbconfig.js
@@ -0,0 +1,4 @@
+module.exports={
+    user:'bigdata', password:'bigdata',connectionString:'13.124.192.232:1521/XE'
+}
+
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const  express = require('express');
 const  router = express.Router();
 const html ='text/html; charset=utf-8';  //아래 텍스트를 사용할거니까 써준다.
 const path = require('path');
+const oracledb=require('oracledb');
+const dbconfig=require('../dbconfig');
+const {query} = require("express");
+
 // show index page
 
 router.get('/',(req, res)=>{    //서버 요청 app-> router
@@ -19,20 +23,50 @@ router.get('/sungjuk',(req, res)=>{    //서버 요청 app-> router
 
 });
 
-router.post('/sungjuk',(req, res)=>{    //서버 요청 app-> router
+router.post('/sungjuk',async (req, res)=>{    //서버 요청 app-> router
     //폼으로 전송된 데이터들은 req.body, req.body.폼 이름 등으로 확인 가능  - 왜냐면 req에 전송되니까.- json 형식으로하면 -정상적으로 인식함
     
     //console.log(req.body);
     //console.log(req.body.name,req.body.kor,req.body.eng,req.body.mat);
     let {name,kor,eng,mat}=req.body;
-    console.log(name,kor,eng,mat);
-    res.redirect(304,'/');
+    kor=parseInt(kor);
+    eng=parseInt(eng);
+    mat=parseInt(mat);
+    console.log(name,kor,eng,mat);//데이터 받아아서 초기화 함
+
 
    let [tot, avg ,grd] =[kor+eng+mat,(kor+eng+mat)/3,'가'];
    console.log(tot,avg,grd);
 
    //데이터베이스 처리 - sungjuk 테이블에 insert
 
+
+
+    let conn=null;
+    let sql='insert into sungjuk'+
+        '(sjno,name,kor,eng,mat ,tot,avg,grd)'+
+        'values(sjno.nextval,:1,:2,:3,:4,:5,:6,:7)';
+    let params=[name,kor,eng,mat,tot,avg,grd];
+
+    try{
+        oracledb.initOracleClient(
+            {libDir:'C:/Java/instantclient_19_17'}
+        ) //처음 한번만 실행되게 해야하는데 이게 입력할때마다 실행 되어서 오류가 난다.
+        conn=await oracledb.getConnection(dbconfig);
+        let result= conn.execute(sql,params);
+        conn.commit();
+        console.log(name,kor,eng,mat)
+
+    }catch (e){
+        console.log(e)
+    }finally {
+        if(conn){
+            try {await conn.close()}catch (e){console.log(e)}
+        }
+    }
+
+
+    res.redirect(304,'/');
 
 });
 
